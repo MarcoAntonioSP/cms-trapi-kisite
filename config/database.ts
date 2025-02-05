caf14e303cb8b7b7f64aaab0f65b4d146d428632
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2025-02-04 21:08:58 -0300

    ajustdatabase

--- config/database.ts
+++ config/database.ts
@@ -1,18 +1,18 @@
-    // strapi-api/config/database.js
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
-        },
-        debug: false,
+// strapi-api/config/database.ts
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('PGHOST', env('DATABASE_HOST')),         // Ex: "railway_private_domain" ou similar
+      port: env.int('PGPORT', 5432),                       // Geralmente 5432
+      database: env('POSTGRES_DB', env('DATABASE_NAME')),  // Ex: "railway"
+      user: env('POSTGRES_USER', env('DATABASE_USERNAME')), // Ex: "postgres"
+      password: env('POSTGRES_PASSWORD', env('DATABASE_PASSWORD')), // Senha conforme Railway
+      schema: env('DATABASE_SCHEMA', 'public'),          // Geralmente "public"
+      ssl: {
+        rejectUnauthorized: false,                       // Necessário se o Railway exigir SSL
       },
-    });
+    },
+    debug: false,
+  },
+});
